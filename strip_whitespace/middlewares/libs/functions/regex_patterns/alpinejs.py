82078746d379efe07e60b756173e164f3cb9a352
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-11-20 20:00:31 +0600

    Update : Add more line break

--- strip_whitespace/middlewares/libs/functions/regex_patterns/alpinejs.py
+++ strip_whitespace/middlewares/libs/functions/regex_patterns/alpinejs.py
@@ -4,10 +4,28 @@ import re
 X_INIT_PATTERN = re.compile(r"(?<=x-init=[\"\`])([^[\"\`\]]+)")
 X_EFFECT_PATTERN = re.compile(r"(?<=x-effect=[\"\`])([^[\"\`\]]+)")
 
-# Click Related Pattern
-CLICK_PATTERN = re.compile(r"(?<=@click=[\"\`])([^[\"\`\]]+)")
-CLICK_PREVENT_PATTERN = re.compile(r"(?<=[@click.prevent]=[\"\`])([^[\"\`\]]+)")
+# Click Related Pattern.
+# Mapped from => https://alpinejs.dev/directives/on#window
+CLICK_PATTERN = re.compile(r"(?<=click=[\"\`])([^[\"\`\]]+)")
+CLICK_PREVENT_PATTERN = re.compile(r"(?<=[click.prevent]=[\"\`])([^[\"\`\]]+)")
+CLICK_STOP_PATTERN = re.compile(r"(?<=[click.stop]=[\"\`])([^[\"\`\]]+)")
+CLICK_OUTSIDE_PATTERN = re.compile(r"(?<=[click.outside]=[\"\`])([^[\"\`\]]+)")
+CLICK_WINDOW_PATTERN = re.compile(r"(?<=[click.window]=[\"\`])([^[\"\`\]]+)")
+CLICK_ONCE_PATTERN = re.compile(r"(?<=[click.document]=[\"\`])([^[\"\`\]]+)")
+CLICK_DEBOUNCE_PATTERN = re.compile(r"(?<=[click.debounce]=[\"\`])([^[\"\`\]]+)")
+CLICK_THROTTLE_PATTERN = re.compile(r"(?<=[click.throttle]=[\"\`])([^[\"\`\]]+)")
+CLICK_SELF_PATTERN = re.compile(r"(?<=[click.self]=[\"\`])([^[\"\`\]]+)")
+CLICK_CAMEL_PATTERN = re.compile(r"(?<=[click.camel]=[\"\`])([^[\"\`\]]+)")
+CLICK_DOT_PATTERN = re.compile(r"(?<=[click.dot]=[\"\`])([^[\"\`\]]+)")
+CLICK_PASSIVE_PATTERN = re.compile(r"(?<=[click.passive]=[\"\`])([^[\"\`\]]+)")
+
 
 # Mouse movement Pattern
-MOUSE_ENTER_PATTERN = re.compile(r"(?<=@mouseenter=[\"\`])([^[\"\`\]]+)")
-MOUSE_LEAVE_PATTERN = re.compile(r"(?<=@mouseleave=[\"\`])([^[\"\`\]]+)")
+# Mapped from => https://www.w3schools.com/jsref/obj_mouseevent.asp
+MOUSE_ENTER_PATTERN = re.compile(r"(?<=mouseenter=[\"\`])([^[\"\`\]]+)")
+MOUSE_LEAVE_PATTERN = re.compile(r"(?<=mouseleave=[\"\`])([^[\"\`\]]+)")
+# MOUSE_CONTEXT_MENU_PATTERN = re.compile(r"(?<=mousecontextmenu=[\"\`])([^[\"\`\]]+)")
+MOUSE_OUT_PATTERN = re.compile(r"(?<=mouseout=[\"\`])([^[\"\`\]]+)")
+MOUSE_OVER_PATTERN = re.compile(r"(?<=mouseover=[\"\`])([^[\"\`\]]+)")
+MOUSE_UP_PATTERN = re.compile(r"(?<=mouseup=[\"\`])([^[\"\`\]]+)")
+MOUSE_MOVE_PATTERN = re.compile(r"(?<=mousemove=[\"\`])([^[\"\`\]]+)")
--- strip_whitespace/middlewares/libs/html/line_break.py
+++ strip_whitespace/middlewares/libs/html/line_break.py
@@ -3,22 +3,32 @@ This module adds line break to html_files
 
 """
 from ..functions.regex import replace_regex
-from ..functions.regex_patterns import (
-    X_INIT_PATTERN,
-    X_EFFECT_PATTERN,
-    CLICK_PATTERN,
-    MOUSE_ENTER_PATTERN,
-    MOUSE_LEAVE_PATTERN,
-    CLICK_PREVENT_PATTERN,
-)
+from ..functions.regex_patterns import *
 
 
 def add_line_break(html: str) -> str:
     html = replace_regex(X_INIT_PATTERN, html)
     html = replace_regex(X_EFFECT_PATTERN, html)
+
+    # Mouse Click Pattern
     html = replace_regex(CLICK_PATTERN, html)
+    html = replace_regex(CLICK_PREVENT_PATTERN, html)
+    html = replace_regex(CLICK_STOP_PATTERN, html)
+    html = replace_regex(CLICK_OUTSIDE_PATTERN, html)
+    html = replace_regex(CLICK_WINDOW_PATTERN, html)
+    html = replace_regex(CLICK_ONCE_PATTERN, html)
+    html = replace_regex(CLICK_DEBOUNCE_PATTERN, html)
+    html = replace_regex(CLICK_THROTTLE_PATTERN, html)
+    html = replace_regex(CLICK_SELF_PATTERN, html)
+    html = replace_regex(CLICK_CAMEL_PATTERN, html)
+    html = replace_regex(CLICK_DOT_PATTERN, html)
+    html = replace_regex(CLICK_PASSIVE_PATTERN, html)
+
+    # Mouse movement Pattern
     html = replace_regex(MOUSE_ENTER_PATTERN, html)
     html = replace_regex(MOUSE_LEAVE_PATTERN, html)
-    html = replace_regex(CLICK_PREVENT_PATTERN, html)
-
+    html = replace_regex(MOUSE_OUT_PATTERN, html)
+    html = replace_regex(MOUSE_OVER_PATTERN, html)
+    html = replace_regex(MOUSE_UP_PATTERN, html)
+    html = replace_regex(MOUSE_MOVE_PATTERN, html)
     return html
