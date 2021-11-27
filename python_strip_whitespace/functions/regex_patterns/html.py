aa3d5bad7ae09d9566c9686ce52b82827eaceb06
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-11-27 10:48:11 +0600

    Unstable update

--- python_strip_whitespace/functions/regex_patterns/html.py
+++ python_strip_whitespace/functions/regex_patterns/html.py
@@ -2,5 +2,5 @@ import re
 from typing import Pattern
 
 NEW_LINE_REPLACE_PATTERN: Pattern = re.compile(
-    r"(?!^)(?<![\s{,\"\`;])\n(?!^(\s\S*($|[\"\`)$]|)}))(?![\w\s]*[\)])"
+    r"(?!^)(?<![\s{,\"\`;:])\n(?!^(\s\S*($|[$])}))(?![\w\s]*[\"\})])" 
 )
