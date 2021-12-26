5125df4266b90742cee5451c886d7c1981dc2fae
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-12-26 20:18:17 +0600

    Monkeypatch

--- python_strip_whitespace/functions/regex.py
+++ python_strip_whitespace/functions/regex.py
@@ -1,18 +1,22 @@
 import typing as t
-from re import findall, sub
+from re import findall
+import minify_html
 
-from .regex_patterns import (
-    NEW_LINE_REPLACE_PATTERN,
-    JAVASCRIPT_DICTIONARY_REGEX_PATTERN,
-)
 
-JS_RESERVED_WORDS = [
+JS_RESERVED_WORDS_BEFORE: t.List = [
     "try",
     "break",
     "finally",
     "switch",
     "typeof",
     "while",
+    "await",
+    "continue",
+    "anime({",
+]
+
+JS_RESERVED_WORDS_AFTER: t.List = [
+    "})",
 ]
 
 
@@ -20,12 +24,20 @@ def replace_regex(RE_PATTERN: t.Pattern, html: str) -> str:
     regex_occurances: t.List = findall(RE_PATTERN, html)
     replaced_regex_occurances: t.List = []
 
-    for i in regex_occurances:
-        json_occurances: t.List = findall(JAVASCRIPT_DICTIONARY_REGEX_PATTERN, i)
-        
+    for i in JS_RESERVED_WORDS_BEFORE:
+        JS_RESERVED_WORDS_BEFORE.remove(i)
+        html = html.replace(i, f";{i}")
 
-        substituated_regex = sub(NEW_LINE_REPLACE_PATTERN, r";", i)
-        replaced_regex_occurances.append(substituated_regex)
+    for i in JS_RESERVED_WORDS_AFTER:
+        JS_RESERVED_WORDS_AFTER.remove(i)
+        html = html.replace(i, f"{i};")
+
+    for i in regex_occurances:
+        minified_js = minify_html.minify(
+            i,
+            minify_js=True,
+        )
+        replaced_regex_occurances.append(minified_js)
 
     for i in regex_occurances:
         regex_index = regex_occurances.index(i)
--- python_strip_whitespace/functions/regex_patterns/html.py
+++ python_strip_whitespace/functions/regex_patterns/html.py
@@ -4,5 +4,3 @@ from typing import Pattern
 NEW_LINE_REPLACE_PATTERN: Pattern = re.compile(
     r"(?!^)(?<![\s{,\"\`;])\n(?!^(\s\S*($|[\"\`)$]|)}))(?![\w\s]*[\)])"
 )
-
-JAVASCRIPT_DICTIONARY_REGEX_PATTERN: Pattern = compile(r"[^{]*{\s*([^}]+)\s*}")
